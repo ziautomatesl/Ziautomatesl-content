218477d5d8c5d23e4b0f9137576beeb229d770db
Ziautomatesl <ziautomate.sl@gmail.com>
2026-05-31 14:34:33 +0200

    fix: eliminar import de ZiaVideo borrado de Root.tsx

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Composition, CalculateMetadataFunction } from "remotion";
-import { ZiaVideo, ZiaVideoProps, defaultZiaVideoProps } from "./ZiaVideo";
+import { Composition } from "remotion";
 import { ZiaCarousel, ZiaCarouselProps, defaultZiaCarouselProps } from "./ZiaCarousel";
 import { ZiaStory, ZiaStoryProps, defaultZiaStoryProps } from "./ZiaStory";
 
@@ -8,21 +7,8 @@ const FPS = 30;
 const CAROUSEL_FRAMES = 6 * 140 - 5 * 25; // 715
 const STORY_FRAMES = 300; // 10s
 
-const calculateVideoMetadata: CalculateMetadataFunction<ZiaVideoProps> = async ({ props }) => ({
-  durationInFrames: Math.ceil(props.durationInSeconds * FPS),
-});
-
 export const RemotionRoot: React.FC = () => (
   <>
-    <Composition
-      id="ZiaVideo"
-      component={ZiaVideo}
-      fps={24}
-      width={1080}
-      height={1920}
-      defaultProps={defaultZiaVideoProps}
-      calculateMetadata={calculateVideoMetadata}
-    />
     <Composition
       id="ZiaCarousel"
       component={ZiaCarousel}
